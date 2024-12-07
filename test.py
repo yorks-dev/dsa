fa8c40b002fca999d0c9ac072e84c766214392c6
Ayush Dutta <yorksdev@gmail.com>
2024-12-07 11:30:37 +0530

    Python New DSA addition

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ print(list(map(lambda x: x**2 if x % 2 == 0 else x, array)))
 
 array3 = [(1, 4), (3, 2), (5, 2), (0, 6)]
 print(list(map(lambda x: (x[1], x[0]), array3)))
-print(sorted(array3, key= lambda x :(x[1], -x[0])))
-print(max(array3, key=lambda x : x[1]))
+print(sorted(array3, key=lambda x: (x[1], -x[0])))
+print(max(array3, key=lambda x: x[1]))
